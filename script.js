fed689d455dc99885b001e522c04af9f836b7ae7
kevth22 <kevteha@gmail.com>
2026-04-30 11:55:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ const wartendeSpieler = spieler.filter((person) => person.bezahlt !== true);
 const belegte = Math.min(bezahlteSpieler.length, maxPlaetze);  
 const wartend = wartendeSpieler.length;  
   
-document.getElementById("belegt").textContent = belegte + " / " + maxPlaetze;  
+document.getElementById("belegt").innerHTML = belegte + ' / <span class="infinity">∞</span>';
 document.getElementById("wartend").textContent = wartend;  
   
 document.getElementById("barBelegt").style.width = (belegte / maxPlaetze * 100) + "%";  
