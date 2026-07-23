a166959b53420514c84fcbed1ae2012c7a9e1ad9
kevth22 <kevteha@gmail.com>
2026-07-23 13:43:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDtQ3pECcZETIoI4QTV5G-7_QcoRvVGHL4",
+  apiKey: "AIzaSyDtQ3pECcZEIloI4QTV5G-7_QcoRvVGHL4",
   authDomain: "dannistreffturnier.firebaseapp.com",
   projectId: "dannistreffturnier",
   storageBucket: "dannistreffturnier.firebasestorage.app",
